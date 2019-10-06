d1cfe907f67a20cff92b777a107f9b95082b5f88
Ole Kristian Losvik <ole@losol.no>
2019-10-06 10:38:55 +0200

    Added css extract

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,13 +7,33 @@ module.exports = {
     filename: 'app.js',
     path: path.resolve(__dirname, '../dist')
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
   module: {
     rules: [
         {
         test: /\.(scss)$/,
         use: [{
             loader: 'style-loader', // inject CSS to page
-        }, {
+        }, 
+        {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+        
+        {
             loader: 'css-loader', // translates CSS into CommonJS modules
         }, {
             loader: 'postcss-loader', // Run postcss actions
